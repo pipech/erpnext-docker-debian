3b98c0dda5b87ab067ad5ccfd6242955d4a4e129
pipeCh <pipech@gmail.com>
2018-02-06 22:30:37 +0700

    more refactoring and cleaning

--- conf/frappe-docker-conf/init.sh
+++ conf/frappe-docker-conf/init.sh
@@ -3,23 +3,18 @@
 # turn on debug mode
 set -x
 
-# variable
-benchUser=frappe
-benchName=bench
-benchWD=/home/$benchUser/$benchName
-benchNewSiteName=prdsite1.local
+# env has been set from dockerfile
+benchWD=/home/$systemUser/$benchName
 
 cd $benchWD
 
 echo "----------------------- [ remove old site ] ---------------------------------"
 cd sites
-rm -rf site1.local
+rm -rf $siteName
 cd ..
 
 echo "----------------------- [ config bench ] ---------------------------------"
-
 cp ~/frappe-docker-conf/common_site_config_docker.json $benchWD/sites/common_site_config.json
-
 bench set-mariadb-host mariadb
 
 echo "----------------------- [ create new site ] ---------------------------------"
