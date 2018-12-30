9e687db8f2c51dcb1ea8fef41741b84e695bf561
pipeCh <pipech@gmail.com>
2018-12-30 14:07:53 +0700

    fixed echo

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo ("# define image abbreviation")
-echo ("## python")
-echo ("python_version: ${python_version}")
+echo "# define image abbreviation"
+echo "## python"
+echo "python_version: ${python_version}"
 python_version_trailing="${python_version: -1}"
-echo ("python_version_trailing: ${python_version_trailing}")
+echo "python_version_trailing: ${python_version_trailing}"
 if [ python_version_trailing == "n" ]; then
     python_abbr="py2"
 elif [ python_version_trailing == "3" ]; then
@@ -13,12 +13,12 @@ else
     echo "ERROR! : Python version error"
     exit 1
 fi
-echo ("python_abbr: ${python_abbr}")
+echo "python_abbr: ${python_abbr}"
 
-echo ("## branch")
-echo ("app_branch: ${app_branch}")
+echo "## branch"
+echo "app_branch: ${app_branch}"
 branch_abbr="${app_branch::3}"
-echo ("branch_abbr: ${branch_abbr}")
+echo "branch_abbr: ${branch_abbr}"
 
 # define image tag and container
 docker_img_tag="${branch_abbr}-${python_abbr}-latest"
