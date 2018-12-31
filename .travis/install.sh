5ecdf5d24e212245fd9b113a8bd05ecee7fe9639
pipeCh <pipech@gmail.com>
2018-12-31 08:48:23 +0700

    test build & push

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "# define image abbreviation"
-echo "## python"
+# define image abbreviation
+## python
 echo "python_version: ${python_version}"
 python_version_trailing="${python_version: -1}"
 echo "python_version_trailing: ${python_version_trailing}"
@@ -15,7 +15,7 @@ else
 fi
 echo "python_abbr: ${python_abbr}"
 
-echo "## branch"
+## branch
 echo "app_branch: ${app_branch}"
 branch_abbr="${app_branch::3}"
 echo "branch_abbr: ${branch_abbr}"
@@ -30,3 +30,30 @@ docker build -t "${docker_img}:${docker_img_tag}" \
     --build-arg pythonVersion="${python_version}" \
     --build-arg appBranch="${app_branch}" \
     .
+
+# run container
+docker run -d -p 8000:8000 -p 9000:9000 --name "${docker_container_name}" "${docker_img}:${docker_img_tag}"
+
+# waiting for container to start
+sleep 120s
+
+# debug
+## docker logs
+docker logs "${docker_container_name}"
+## docker inspect
+docker inspect "${docker_container_name}"
+
+# test html response
+html_response = $(docker exec "${docker_container_name}" python test_server.py)
+echo "${html_response}"
+
+# remove container
+docker rm -f "${docker_container_name}"
+
+# push image
+if [ "$html_response" == "200" ]; then
+    docker push "${docker_img}:${docker_img_tag}"
+else
+    echo "ERROR! : Html reponse is not 200"
+    exit 1
+fi
