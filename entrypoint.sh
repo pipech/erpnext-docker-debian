ee1f2d52a8d7059f59f0e592295dc3abaa7e934d
pipeCh <pipech@gmail.com>
2018-12-31 10:25:03 +0700

    mysql fixing attempt

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,9 @@
 
 set -euxo pipefail
 
+# work around for mysql failed to start
+# https://github.com/moby/moby/issues/34390
+find /var/lib/mysql/mysql -exec touch -c -a {} +
+
 sudo service mysql start
 bench start
