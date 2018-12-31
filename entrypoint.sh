36f807b3461cafb065e93a978385fbbac7e7fec2
pipeCh <pipech@gmail.com>
2018-12-31 10:37:38 +0700

    fixed permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # work around for mysql failed to start
 # https://github.com/moby/moby/issues/34390
-find /var/lib/mysql/mysql -exec touch -c -a {} +
+sudo find /var/lib/mysql/mysql -exec touch -c -a {} +
 
 sudo service mysql start
 bench start
