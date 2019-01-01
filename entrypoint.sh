ba4f7c20bb04692940527bfbce7b24f96852fdce
pipeCh <pipech@gmail.com>
2019-01-01 20:39:52 +0700

    try windows build environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,5 @@
 
 set -euxo pipefail
 
-# work around for mysql failed to start
-# https://github.com/moby/moby/issues/34390
-sudo find /var/lib/mysql/mysql -exec touch -c -a {} +
-
 sudo service mysql start
 bench start
