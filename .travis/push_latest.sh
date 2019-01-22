d6777a0d74e182625c0fb3a8af0632e530ac3f37
pipeCh <pipech@gmail.com>
2019-01-22 09:38:51 +0700

    use start script from docker image

--- .travis/push_latest.sh
+++ .travis/push_latest.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "# starting container"
-docker run -d -p 8000:8000 -p 9000:9000 --name "${docker_container_name}" "${docker_img}:${docker_img_tag}" sudo mysqld start
-docker exec -d "${docker_container_name}" bench start
+docker run -d -p 8000:8000 -p 9000:9000 --name "${docker_container_name}" "${docker_img}:${docker_img_tag}"
 
 echo "# waiting for container to start"
-sleep 120s
+sleep 180s
 
 echo "# debug"
+echo "## docker list"
+docker ps -a
 echo "## docker logs"
 docker logs "${docker_container_name}"
 echo "## docker inspect"
