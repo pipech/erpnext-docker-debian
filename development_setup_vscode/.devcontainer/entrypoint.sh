84bb6677125fb2f2b64b778f2892daad3784f0bb
unknown <pipech@gmail.com>
2019-06-04 10:58:41 +0700

    change chmod location

--- development_setup_vscode/.devcontainer/entrypoint.sh
+++ development_setup_vscode/.devcontainer/entrypoint.sh
@@ -2,10 +2,5 @@
 
 set -euxo pipefail
 
-# I don't think we need to concern about security in dev container.
-# prevent - vscode error when trying to attatch to container
-# >> mkdir: cannot create directory ‘/root’: Permission denied
-sudo chmod 777 /root
-
 sudo service mysql start
 bench start
