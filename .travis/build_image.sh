27953cb6b0a535ba02cfa9362d602a5be1feafe3
Poranut Chollavorn <pipech@gmail.com>
2023-11-21 22:53:55 +0700

    fix: remove travis

--- .travis/build_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "# build image"
-docker build -t "${docker_img}:${docker_img_tag}" \
-    --no-cache \
-    --build-arg pythonVersion="${python_version}" \
-    --build-arg appBranch="${app_branch}" \
-    .
