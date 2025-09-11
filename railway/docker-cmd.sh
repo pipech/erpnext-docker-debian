55bd2a6e79f146608f1a48bb2f241f89cc41b9d6
Poranut Chollavorn <pipech@gmail.com>
2025-09-11 20:19:29 +0700

    feat: update cmd script

--- railway/docker-cmd.sh
+++ railway/docker-cmd.sh
@@ -5,7 +5,7 @@ echo "-> Bench build"
 ln -s /home/$systemUser/$benchFolderName/built_sites/assets /home/$systemUser/$benchFolderName/sites/assets
 
 echo "-> Bursting env into config"
-envsubst '$DOMAIN_PANICH_ADMIN' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
+envsubst '$FRAPPE_DOMAIN_NAME' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
 envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/$systemUser/temp_supervisor.conf > /home/$systemUser/supervisor.conf
 
 echo "-> Starting nginx"
