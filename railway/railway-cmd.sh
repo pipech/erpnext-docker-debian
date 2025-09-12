19dab55d3bc8229fd4d3b4da611c81c0e56b27dc
Poranut Chollavorn <pipech@gmail.com>
2025-09-12 09:52:13 +0700

    feat(railway): add railway setup script

---
+++ railway/railway-cmd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo "-> Linking assets"
+ln -s /home/$systemUser/$benchFolderName/built_sites/assets /home/$systemUser/$benchFolderName/sites/assets
+
+echo "-> Bursting env into config"
+sudo envsubst '$RFP_DOMAIN_NAME' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
+sudo envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/$systemUser/temp_supervisor.conf > /home/$systemUser/supervisor.conf
+
+echo "-> Starting nginx"
+sudo nginx
+
+echo "-> Starting supervisor"
+sudo /usr/bin/supervisord -c /home/$systemUser/supervisor.conf
