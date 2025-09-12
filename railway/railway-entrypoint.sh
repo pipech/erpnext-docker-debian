dc78f0c83280a7ea8d0cab698ce68b27ccf0853e
Poranut Chollavorn <pipech@gmail.com>
2025-09-12 13:14:36 +0700

    fix(railway): docker script

--- railway/railway-entrypoint.sh
+++ railway/railway-entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "-> Linking assets"
-ln -s /home/$systemUser/$benchFolderName/built_sites/assets /home/$systemUser/$benchFolderName/sites/assets
-ln -s /home/$systemUser/$benchFolderName/built_sites/apps.json /home/$systemUser/$benchFolderName/sites/apps.json
-ln -s /home/$systemUser/$benchFolderName/built_sites/apps.txt /home/$systemUser/$benchFolderName/sites/apps.txt
+ln -s /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets
+ln -s /home/frappe/bench/built_sites/apps.json /home/frappe/bench/sites/apps.json
+ln -s /home/frappe/bench/built_sites/apps.txt /home/frappe/bench/sites/apps.txt
 
 exec "$@"
