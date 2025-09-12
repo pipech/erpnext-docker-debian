961837b9fc0b818d56091842ac3abfd685023914
Poranut Chollavorn <pipech@gmail.com>
2025-09-12 14:53:26 +0700

    fix(railway): supervisor config

--- railway/railway-entrypoint.sh
+++ railway/railway-entrypoint.sh
@@ -5,8 +5,8 @@ echo "-> Set ownership of sites folder"
 chown frappe:frappe /home/frappe/bench/sites
 
 echo "-> Linking assets"
-ln -sf /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets
-ln -sf /home/frappe/bench/built_sites/apps.json /home/frappe/bench/sites/apps.json
-ln -sf /home/frappe/bench/built_sites/apps.txt /home/frappe/bench/sites/apps.txt
+su frappe -c "ln -sf /home/frappe/bench/built_sites/assets /home/frappe/bench/sites/assets"
+su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.json /home/frappe/bench/sites/apps.json"
+su frappe -c "ln -sf /home/frappe/bench/built_sites/apps.txt /home/frappe/bench/sites/apps.txt"
 
 exec "$@"
